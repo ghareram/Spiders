acf33036bd77c8b34473104e46c9f6543e55dad7
ghareram <hareram@idntyti.com>
2022-04-04 18:12:26 +0530

    MySQL db to CSV - vivanuncios(AMX 2nd Source) -Details with Nextpage

---
+++ vivanuncios_dbtocsv.py
@@ -0,0 +1,41 @@
+import pymysql
+import csv
+import sys
+
+db_opts = {
+    'user': 'hare',
+    'password': 'Ram@1234',
+    'host': 'localhost',
+    'database': 'vivanuncios'
+}
+
+db = pymysql.connect(**db_opts)
+cur = db.cursor()
+
+sql = 'SELECT * from vivanuncios'
+csv_file_path = '/home/hareram/vivanuncios/vivanuncios/spiders/vivanuncios_dbtocsv.csv'
+
+try:
+    cur.execute(sql)
+    rows = cur.fetchall()
+finally:
+    db.close()
+
+if rows:
+    result = list()
+
+    column_names = list()
+    for i in cur.description:
+        column_names.append(i[0])
+
+    result.append(column_names)
+    for row in rows:
+        result.append(row)
+
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in result:
+            csvwriter.writerow(row)
+else:
+    sys.exit("No rows found for query: {}".format(sql))
+
